2a6a5cd00d5c9609d9bb0bc1f2d7941e2b996c37
Aleksandar Grujovic <aleksbmr@gmail.com>
2024-01-21 21:00:11 +0100

    centered and added close button

--- testing-area/modal/modal.js
+++ testing-area/modal/modal.js
@@ -2,6 +2,18 @@ var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("myBtn");
 
+var span = document.getElementsByClassName("close-button");
+
 btn.onclick = function() {
     modal.style.display = "block";
-};
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
