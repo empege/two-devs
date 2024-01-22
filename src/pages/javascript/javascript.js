656e548c51931dda7321fb7bdb29062cd0124703
Ivan <ivan.mitov@hotmail.com>
2024-01-22 13:13:14 +0100

    Multiple pages webpack

---
+++ src/pages/javascript/javascript.js
@@ -0,0 +1,14 @@
+// Styles
+import '../../styles/main.scss';
+
+// Assets
+import logo from '../../assets/logo.png'
+
+// Templates
+import headerTemplate from "../../components/header/header.hbs";
+
+// JS
+import '../../components/header/header'
+
+// Register components
+document.getElementById('HEADER').innerHTML = headerTemplate({ logo, title: 'JAVASCRIPT PAGE' })
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,27 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const pages = ['html', 'css', 'javascript', 'about']
+let htmlWebpackPluginPages = pages.map((page) => {
+  return new HtmlWebpackPlugin({
+    title: `⚀ Two Devs - ${page} ⚀`,
+    filename: `${page}.html`,
+    template: `src/pages/${page}/${page}.hbs`,
+    chunks: [`${page}`],
+  })
+})
+
 module.exports = {
   mode: 'development',
   entry: {
-    main: path.resolve(__dirname, 'src/main.js') // If entry is an object, key is name of bundled file, and you can use it as [name] in output for example ⬇️
+    index: path.resolve(__dirname, 'src/index.js'), // If entry is an object, key is name of bundled file, and you can use it as [name] in output for example ⬇️
+    html: path.resolve(__dirname, 'src/pages/html/html.js')
   },
-  // entry: path.resolve(__dirname, 'src/main.js'), // Simpler way to define entry, without object
+  // entry: path.resolve(__dirname, 'src/index.js'), // Simpler way to define entry, without object
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: '[name].[contenthash].js', // [name] = main, [contenthash] creates hash on every build so no caching issues, . is not needed, I just like it there :)
-    // filename: 'main.js' // Simpler way to define it
+    filename: '[name].[contenthash].js', // [name] = index, [contenthash] creates hash on every build so no caching issues, . is not needed, I just like it there :)
+    // filename: 'index.js' // Simpler way to define it
     clean: true, // Delete previous files so hashed files don't pile up
     assetModuleFilename: '[name][ext]' // Keep name of resources (images etc) and extension instead of hashing it by default
   },
@@ -21,7 +32,7 @@ module.exports = {
       directory: path.resolve(__dirname, 'build')
     },
     port: 3000, // Default 8080
-    open: true, // Open page automatically
+    // open: true, // Open page automatically
     hot: true, // Hot Module Replacement - See all changes immediately on save, without restarting whole application
     compress: true, // ??? gzip conversion so quicker data/file sending ???
     historyApiFallback: true // ??? when we go to route that doesn't exist we fallback to index.html ???
@@ -58,12 +69,14 @@ module.exports = {
     ]
   },
   plugins: [
-    // Usefull for not having index.html in build. I wanna delete whole build and still have index.html remade, and also I want to add some things in there that always remain when being recreated.
+    // new BundleAnalyzerPlugin(), // If you want to see visual representation of what takes how much space etc
+
+    // HtmlWebpackPlugin - Usefull for not having index.html in build. I wanna delete whole build and still have index.html remade, and also I want to add some things in there that always remain when being recreated.
     new HtmlWebpackPlugin({
       title: '⚀ Two Devs ⚀',
       filename: 'index.html',
-      template: 'src/index.hbs'
-    }),
-    // new BundleAnalyzerPlugin(), // If you want to see visual representation of what takes how much space etc
-  ]
+      template: 'src/index.hbs',
+      chunks: ['index'],
+    })
+  ].concat(htmlWebpackPluginPages)
 }
